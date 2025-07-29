1739e180cc678ba5c7453851a8eb3626ac508c1c
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2025-07-29 16:04:09 +0100

    added entity for maat refs to extract table

---
+++ maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/billing/entity/MaatReferenceEntity.java
@@ -0,0 +1,37 @@
+package uk.gov.justice.laa.maat.scheduled.tasks.billing.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+@IdClass(MaatReferenceId.class)
+@Table(name = "MAAT_REFS_TO_EXTRACT", schema = "TOGDATA")
+public class MaatReferenceEntity {
+
+    @Id
+    @Column(name = "MAAT_ID")
+    private Integer maatId;
+
+    @Id
+    @Column(name = "APPL_ID")
+    private Integer applicantId;
+
+    @Id
+    @Column(name = "APHI_ID")
+    private Integer applicantHistoryId;
+}
