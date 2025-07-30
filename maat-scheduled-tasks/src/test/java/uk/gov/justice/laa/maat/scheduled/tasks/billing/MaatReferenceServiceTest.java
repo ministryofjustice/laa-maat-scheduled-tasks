9eb19b28b6c74583f4e65057942a796b14f1176d
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2025-07-30 11:32:53 +0100

    added unit tests for populat maat refs service

---
+++ maat-scheduled-tasks/src/test/java/uk/gov/justice/laa/maat/scheduled/tasks/billing/MaatReferenceServiceTest.java
@@ -0,0 +1,44 @@
+package uk.gov.justice.laa.maat.scheduled.tasks.billing;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.justice.laa.maat.scheduled.tasks.billing.repository.MaatReferenceRepository;
+import uk.gov.justice.laa.maat.scheduled.tasks.billing.service.MaatReferenceService;
+import uk.gov.justice.laa.maat.scheduled.tasks.exception.RecordsAlreadyExistException;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@ExtendWith(MockitoExtension.class)
+class MaatReferenceServiceTest {
+
+    @Mock
+    private MaatReferenceRepository repository;
+
+    @InjectMocks
+    private MaatReferenceService service;
+
+    @Test
+    void givenTableIsEmpty_whenPopulateMaatReferencesIsInvoked_thenRepositoryIsCalled() {
+        when(repository.count()).thenReturn(Long.valueOf(0));
+
+        service.populateMaatReferences();
+
+        verify(repository).populateMaatReferences();
+    }
+
+    @Test
+    void givenTableIsPopulated_whenPopulateMaatReferencesIsInvoked_thenExceptionIsThrown() {
+        when(repository.count()).thenReturn(Long.valueOf(666));
+
+        assertThatThrownBy(() -> {
+            service.populateMaatReferences();
+        }).isInstanceOf(RecordsAlreadyExistException.class)
+            .hasMessageContaining("The maat references table is already populated.");
+    }
+
+}
