611200c35ff42e26d814110b10566d16670ff36f
LAA-Dave-Edwards <134281793+LAA-Dave-Edwards@users.noreply.github.com>
2025-07-31 15:58:57 +0100

    Removing apache commons.

--- maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/mapper/RepOrderBillingMapper.java
+++ maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/mapper/RepOrderBillingMapper.java
@@ -1,16 +1,16 @@
 package uk.gov.justice.laa.maat.scheduled.tasks.mapper;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 import uk.gov.justice.laa.maat.scheduled.tasks.dto.RepOrderBillingDTO;
 import uk.gov.justice.laa.maat.scheduled.tasks.entity.RepOrderBillingEntity;
-import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class RepOrderBillingMapper {
     private RepOrderBillingMapper() { }
 
     public static RepOrderBillingDTO mapEntityToDTO(RepOrderBillingEntity repOrderEntity) {
-        Integer magsCourtId = StringUtils.isEmpty(repOrderEntity.getMagsCourtId())
+        Integer magsCourtId = !StringUtils.hasLength(repOrderEntity.getMagsCourtId())
             ? null : Integer.parseInt(repOrderEntity.getMagsCourtId());
 
         return RepOrderBillingDTO.builder()
