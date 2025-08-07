73417218f7617afbc0046bff95b4157adec506eb
Karl Baker <karl.baker@digital.justice.gov.uk>
2025-08-07 10:26:00 +0100

    Add test for resulting dto

--- maat-scheduled-tasks/src/test/java/uk/gov/justice/laa/maat/scheduled/tasks/service/XhibitDataServiceTest.java
+++ maat-scheduled-tasks/src/test/java/uk/gov/justice/laa/maat/scheduled/tasks/service/XhibitDataServiceTest.java
@@ -4,6 +4,9 @@ import static org.hamcrest.Matchers.any;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.nio.charset.StandardCharsets;
@@ -12,12 +15,16 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.util.StringUtils;
+import software.amazon.awssdk.core.BytesWrapper;
+import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
@@ -25,6 +32,7 @@ import software.amazon.awssdk.services.s3.model.S3Object;
 import uk.gov.justice.laa.maat.scheduled.tasks.config.XhibitConfiguration;
 import uk.gov.justice.laa.maat.scheduled.tasks.dto.XhibitRecordSheetDTO;
 import uk.gov.justice.laa.maat.scheduled.tasks.enums.RecordSheetType;
+import uk.gov.justice.laa.maat.scheduled.tasks.matchers.GetObjectRequestArgumentMatcher;
 
 @ExtendWith(MockitoExtension.class)
 class XhibitDataServiceTest {
@@ -46,7 +54,7 @@ class XhibitDataServiceTest {
 
     @BeforeEach
     void beforeEach() {
-        when(xhibitConfiguration.getObjectKeyTrialPrefix()).thenReturn("trial");
+        when(xhibitConfiguration.getObjectKeyTrialPrefix()).thenReturn("trial/");
         when(xhibitConfiguration.getS3DataBucketName()).thenReturn("bucket");
     }
 
@@ -66,6 +74,7 @@ class XhibitDataServiceTest {
             S3Object.builder().key("trial/file1.xml").build(),
             S3Object.builder().key("trial/file2.xml").build()
         ));
+        when(listObjectsV2Response.isTruncated()).thenReturn(false);
         when(s3Client.listObjectsV2(ArgumentMatchers.any(ListObjectsV2Request.class))).thenReturn(listObjectsV2Response);
 
         List<XhibitRecordSheetDTO> expectedRecordSheets = List.of(
@@ -79,11 +88,18 @@ class XhibitDataServiceTest {
                 .build()
         );
 
-        // TODO: Need to set-up the individual requests - may need a custom arg matcher.
-        when(s3Client.getObjectAsBytes()).thenReturn(expectedRecordSheets.get(0).getData().getBytes(
-                StandardCharsets.UTF_8);
-        when(s3Client.getObjectAsBytes()).thenReturn(expectedRecordSheets.get(1).getData().getBytes(
-            StandardCharsets.UTF_8);
+        ResponseBytes<GetObjectResponse> file1ObjectResponse = ResponseBytes.fromByteArray(
+            mock(GetObjectResponse.class),
+            expectedRecordSheets.get(0).getData().getBytes(StandardCharsets.UTF_8)
+        );
+
+        ResponseBytes<GetObjectResponse> file2ObjectResponse = ResponseBytes.fromByteArray(
+            mock(GetObjectResponse.class),
+            expectedRecordSheets.get(1).getData().getBytes(StandardCharsets.UTF_8)
+        );
+
+        doReturn(file2ObjectResponse).when(s3Client).getObjectAsBytes(argThat(new GetObjectRequestArgumentMatcher("trial/file2.xml")));
+        doReturn(file1ObjectResponse).when(s3Client).getObjectAsBytes(argThat(new GetObjectRequestArgumentMatcher("trial/file1.xml")));
 
         List<XhibitRecordSheetDTO> actualRecordSheets = xhibitDataService.getRecordSheets(RecordSheetType.TRIAL);
 
