d0e0b0b5f6b95fbb0e3c1b1a51df03f10d64b841
Adam Lombardi-Barron <Adam.LombardiBarron@justice.gov.uk>
2025-09-03 17:12:36 +0100

    LASB-4404: Improve stored procedure error handling
    Ensure exceptions thrown by StoredProcedureService are caught
    Mark records as errored if the corresponding stored procedure fails
    Abstract stored procedure parameters to be more generic and extensible

--- maat-scheduled-tasks/src/test/java/uk/gov/justice/laa/maat/scheduled/tasks/service/StoredProcedureServiceTest.java
+++ maat-scheduled-tasks/src/test/java/uk/gov/justice/laa/maat/scheduled/tasks/service/StoredProcedureServiceTest.java
@@ -9,15 +9,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.justice.laa.maat.scheduled.tasks.exception.StoredProcedureException;
+import uk.gov.justice.laa.maat.scheduled.tasks.helper.StoredProcedureParameter;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.justice.laa.maat.scheduled.tasks.helper.StoredProcedureParameter.inOutParameter;
+import static uk.gov.justice.laa.maat.scheduled.tasks.helper.StoredProcedureParameter.inputParameter;
+import static uk.gov.justice.laa.maat.scheduled.tasks.helper.StoredProcedureParameter.outputParameter;
 
 @ExtendWith(MockitoExtension.class)
 class StoredProcedureServiceTest {
@@ -79,40 +84,58 @@ class StoredProcedureServiceTest {
     }
 
     @Test
-    void testCallStoredProcedure_withParameters_registeredAndSetSuccessfully() {
+    void testCalledStoredProcedure_withParameters_registeredAndExecutedSuccessfully() {
         String procedureName = "my_procedure";
-        Map<String, Object> inputParams = Map.of("param1", "value1", "param2", 3);
-
-        when(entityManager.createStoredProcedureQuery(procedureName)).thenReturn(storedProcedureQuery);
-
-        storedProcedureService.callStoredProcedure(procedureName, inputParams, Collections.emptyMap());
-
-        verify(storedProcedureQuery).registerStoredProcedureParameter("param1", String.class, ParameterMode.IN);
-        verify(storedProcedureQuery).registerStoredProcedureParameter("param2", Integer.class, ParameterMode.IN);
-        verify(storedProcedureQuery).setParameter("param1", "value1");
-        verify(storedProcedureQuery).setParameter("param2", 3);
-        verify(storedProcedureQuery).execute();
-    }
-
-    @Test
-    void testCallStoredProcedure_WithInAndOutParams_registeredAndSetSuccessfully() {
-        String procedureName = "my_procedure";
-        Map<String, Object> inputParams = Map.of("inParam1", "value1", "inParam2", 3);
-        Map<String, Class<?>> outParams = Map.of("outParam1", String.class, "outParam2", Integer.class);
+        Collection<StoredProcedureParameter<?>> parameters = List.of(
+            inputParameter("inParam1", "value1"),
+            inputParameter("inParam2", 3),
+            outputParameter("outParam1", String.class),
+            outputParameter("outParam2", Integer.class),
+            inOutParameter("inOutParam1", "inOutValue"),
+            inOutParameter("inOutParam2", 1234L)
+        );
 
         when(entityManager.createStoredProcedureQuery(procedureName)).thenReturn(storedProcedureQuery);
         when(storedProcedureQuery.getOutputParameterValue("outParam1")).thenReturn("returnValue");
         when(storedProcedureQuery.getOutputParameterValue("outParam2")).thenReturn(5);
+        when(storedProcedureQuery.getOutputParameterValue("inOutParam1")).thenReturn("inOutValue");
+        when(storedProcedureQuery.getOutputParameterValue("inOutParam2")).thenReturn(1234L);
 
-        storedProcedureService.callStoredProcedure(procedureName, inputParams, outParams);
+        storedProcedureService.callStoredProcedure(procedureName, parameters);
 
         verify(storedProcedureQuery).registerStoredProcedureParameter("inParam1", String.class, ParameterMode.IN);
         verify(storedProcedureQuery).registerStoredProcedureParameter("inParam2", Integer.class, ParameterMode.IN);
         verify(storedProcedureQuery).registerStoredProcedureParameter("outParam1", String.class, ParameterMode.OUT);
         verify(storedProcedureQuery).registerStoredProcedureParameter("outParam2", Integer.class, ParameterMode.OUT);
+        verify(storedProcedureQuery).registerStoredProcedureParameter("inOutParam1", String.class, ParameterMode.INOUT);
+        verify(storedProcedureQuery).registerStoredProcedureParameter("inOutParam2", Long.class, ParameterMode.INOUT);
+
         verify(storedProcedureQuery).setParameter("inParam1", "value1");
         verify(storedProcedureQuery).setParameter("inParam2", 3);
+        verify(storedProcedureQuery).setParameter("inOutParam1", "inOutValue");
+        verify(storedProcedureQuery).setParameter("inOutParam2", 1234L);
+
         verify(storedProcedureQuery).execute();
     }
 
+    @Test
+    void executedStoredProcedure_outputParams_valuesRetrievedForLogging() {
+        String procedureName = "my_procedure";
+        Collection<StoredProcedureParameter<?>> parameters = List.of(
+            inputParameter("inParam1", "value1"),
+            outputParameter("outParam1", String.class),
+            inOutParameter("inOutParam1", "inOutValue")
+        );
+
+        when(entityManager.createStoredProcedureQuery(procedureName)).thenReturn(storedProcedureQuery);
+        when(storedProcedureQuery.getOutputParameterValue("outParam1")).thenReturn("returnValue");
+        when(storedProcedureQuery.getOutputParameterValue("inOutParam1")).thenReturn("inOutValue");
+
+        storedProcedureService.callStoredProcedure(procedureName, parameters);
+
+        verify(storedProcedureQuery).execute();
+        verify(storedProcedureQuery).getOutputParameterValue("outParam1");
+        verify(storedProcedureQuery).getOutputParameterValue("inOutParam1");
+        verify(storedProcedureQuery, never()).getOutputParameterValue("inParam");
+    }
 }
