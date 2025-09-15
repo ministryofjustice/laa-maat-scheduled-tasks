2c4d60997481bfd1a664bdab47180cfb0df9c7c9
Karl Baker <karl.baker@digital.justice.gov.uk>
2025-09-15 10:51:20 +0100

    Refactor ApplicantBillingServiceTest
    This commit refactors the _ApplicantBillingServiceTest_ to rename
    the test methods and move the failure case to be the first test.

--- maat-scheduled-tasks/src/test/java/uk/gov/justice/laa/maat/scheduled/tasks/service/BillingDataFeedLogServiceTest.java
+++ maat-scheduled-tasks/src/test/java/uk/gov/justice/laa/maat/scheduled/tasks/service/BillingDataFeedLogServiceTest.java
@@ -3,10 +3,12 @@ package uk.gov.justice.laa.maat.scheduled.tasks.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -27,11 +29,49 @@ public class BillingDataFeedLogServiceTest {
     private BillingDataFeedLogService billingDataFeedLogService;
 
     @Test
+    void givenNoDataExists_whenGetBillingDataFeedLogsIsInvoked_thenReturnEmptyList() {
+        List<BillingDataFeedLogEntity> records = billingDataFeedLogService.getBillingDataFeedLogs(BillingDataFeedRecordType.APPLICANT);
+
+        assertThat(records).isEmpty();
+    }
+
+    @Test
+    void givenNoMatchingDataExists_whenGetBillingDataFeedLogsIsInvoked_thenReturnEmptyList() {
+        BillingDataFeedLogEntity repOrderBillingEntity = BillingDataFeedLogEntity.builder()
+            .id(1)
+            .recordType(BillingDataFeedRecordType.REP_ORDER.getValue())
+            .payload("")
+            .build();
+
+        lenient().when(billingDataFeedLogRepository.getBillingDataFeedLogEntitiesByRecordType(BillingDataFeedRecordType.REP_ORDER.getValue()))
+            .thenReturn(List.of(repOrderBillingEntity));
+
+        List<BillingDataFeedLogEntity> records = billingDataFeedLogService.getBillingDataFeedLogs(BillingDataFeedRecordType.APPLICANT);
+
+        assertThat(records).isEmpty();
+    }
+
+    @Test
+    void givenMatchingDataExists_whenGetBillingDataFeedLogsIsInvoked_thenReturnsRecords() {
+        BillingDataFeedLogEntity applicantBillingEntity = BillingDataFeedLogEntity.builder()
+            .id(123)
+            .recordType(BillingDataFeedRecordType.APPLICANT.getValue())
+            .payload("")
+            .build();
+
+        when(billingDataFeedLogRepository.getBillingDataFeedLogEntitiesByRecordType(BillingDataFeedRecordType.APPLICANT.getValue()))
+            .thenReturn(List.of(applicantBillingEntity));
+
+        List<BillingDataFeedLogEntity> records = billingDataFeedLogService.getBillingDataFeedLogs(BillingDataFeedRecordType.APPLICANT);
+
+        assertThat(applicantBillingEntity).isEqualTo(records.getFirst());
+    }
+
+    @Test
     void givenValidData_whenSaveBillingDataFeedIsInvoked_thenDataIsSavedToRepository() {
         BillingDataFeedRecordType recordType = BillingDataFeedRecordType.APPLICANT;
-        String payload =
-                "[ApplicantBillingDTO(id=1, firstName='John', lastName='Doe', dob='1983-02-03'," +
-                        " gender='Male', niNumber='SR096795A', dateCreated='2025-01-01', userCreated='test-u')]";
+        String payload = "[ApplicantBillingDTO(id=1, firstName='John', lastName='Doe', dob='1983-02-03'," +
+            " gender='Male', niNumber='SR096795A', dateCreated='2025-01-01', userCreated='test-u')]";
 
         billingDataFeedLogService.saveBillingDataFeed(recordType, payload);
 
@@ -44,8 +84,8 @@ public class BillingDataFeedLogServiceTest {
 
         Long logsDeleted = billingDataFeedLogService.deleteLogsBeforeDate(THRESHOLD_DATE);
 
-        assertThat(logsDeleted).isEqualTo(2L);
         verify(billingDataFeedLogRepository).deleteByDateCreatedBefore(THRESHOLD_DATE);
+        assertThat(logsDeleted).isEqualTo(2L);
     }
 
     @Test
