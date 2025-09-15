52724607788da7f9b332f2c05c85d6baaf4af4fe
Karl Baker <karl.baker@digital.justice.gov.uk>
2025-09-15 14:19:15 +0100

    Add BillingDataFeedLogMapper
    This commit adds a new _BillingDataFeedLogMapper_ for mapping from
    the _BillingDataFeedLogEntity_ class to the respective DTO (deserialising
    the payload within). To achieve this in a way which reduces duplication,
    a new base class _BillingDTO_ has been created and is added to the
    billing DTO classes. If a _JsonProcessingException_ occurs during
    deserialisation then the mapper will return `null` rather than throw an
    exception, allowing all correctly-serialised data to be deserialised.

--- maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/dto/ApplicantHistoryBillingDTO.java
+++ maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/dto/ApplicantHistoryBillingDTO.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ApplicantHistoryBillingDTO {
+public class ApplicantHistoryBillingDTO implements BillingDTO {
 
     @NotNull
     @JsonProperty("id")
