b8c3c8b45806dac3dd392901db347880064bd0dd
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2025-09-18 15:03:03 +0100

    added util class to split lists into batches

--- maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/service/ApplicantBillingService.java
+++ maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/service/ApplicantBillingService.java
@@ -1,6 +1,5 @@
 package uk.gov.justice.laa.maat.scheduled.tasks.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
