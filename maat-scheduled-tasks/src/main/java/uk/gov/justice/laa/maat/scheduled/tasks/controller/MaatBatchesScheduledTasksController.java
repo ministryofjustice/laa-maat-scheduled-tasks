5ac9903f008fa2cc8abfee42a84c576a798b0c87
Ross-Nation <139126932+Ross-Nation@users.noreply.github.com>
2025-09-18 14:35:22 +0100

    Add openapi documentation

--- maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/controller/MaatBatchesScheduledTasksController.java
+++ maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/controller/MaatBatchesScheduledTasksController.java
@@ -1,5 +1,7 @@
 package uk.gov.justice.laa.maat.scheduled.tasks.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
@@ -7,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.justice.laa.maat.scheduled.tasks.annotation.StandardApiResponse;
 import uk.gov.justice.laa.maat.scheduled.tasks.scheduler.MaatBatchesScheduler;
 
 
@@ -15,28 +18,40 @@ import uk.gov.justice.laa.maat.scheduled.tasks.scheduler.MaatBatchesScheduler;
 @RestController
 @Profile("!prod") // Only active in non-prod environments
 @RequestMapping("api/internal/v1/maat-batches")
+@Tag(
+    name="MAAT Batches Scheduled Tasks",
+    description = "Rest API to trigger scheduled tasks related to MAAT Batches"
+)
 public class MaatBatchesScheduledTasksController {
   MaatBatchesScheduler maatBatchesScheduler;
 
   @PostMapping("/lmr-reports")
+  @Operation(description = "Trigger the MAAT Batches Local Management Report scheduled task")
+  @StandardApiResponse
   public ResponseEntity<String> triggerLocalManagementReports() {
     maatBatchesScheduler.executeLocalManagementReports();
     return ResponseEntity.ok("MAAT Batches Local Management Report task triggered.");
   }
 
   @PostMapping("/evidence-reminder")
+  @Operation(description = "Trigger the MAAT Batches Evidence Reminder Letter scheduled task")
+  @StandardApiResponse
   public ResponseEntity<String> triggerEvidenceReminderLetter() {
     maatBatchesScheduler.generateEvidenceReminderLetter();
     return ResponseEntity.ok("MAAT Batches Evidence Reminder Letter task triggered.");
   }
 
   @PostMapping("/deactivate-inactive-users")
+  @Operation(description = "Trigger the MAAT Batches Deactivate Inactive Users scheduled task")
+  @StandardApiResponse
   public ResponseEntity<String> triggerDeactivateInactiveUsers() {
     maatBatchesScheduler.deactivateInactiveUsers();
     return ResponseEntity.ok("MAAT Batches Deactivate Inactive Users task triggered.");
   }
 
   @PostMapping("/financial-assessment-fix")
+  @Operation(description = "Trigger the MAAT Batches Financial Assessment Fix scheduled task")
+  @StandardApiResponse
   public ResponseEntity<String> triggerFinancialAssessmentFix() {
     maatBatchesScheduler.executeFinancialAssessmentFix();
     return ResponseEntity.ok("MAAT Batches Financial Assessment Fix task triggered.");
--- maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/controller/XhibitBatchesScheduledTasksController.java
+++ maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/controller/XhibitBatchesScheduledTasksController.java
@@ -1,5 +1,7 @@
 package uk.gov.justice.laa.maat.scheduled.tasks.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
@@ -7,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.justice.laa.maat.scheduled.tasks.annotation.StandardApiResponse;
 import uk.gov.justice.laa.maat.scheduled.tasks.scheduler.XhibitBatchesScheduler;
 
 
@@ -15,16 +18,24 @@ import uk.gov.justice.laa.maat.scheduled.tasks.scheduler.XhibitBatchesScheduler;
 @RestController
 @Profile("!prod") // Only active in non-prod environments
 @RequestMapping("api/internal/v1/xhibit-batches")
+@Tag(
+    name="Xhibit Batches Scheduled Tasks",
+    description = "Rest API to trigger scheduled tasks related to Xhibit Batches"
+)
 public class XhibitBatchesScheduledTasksController {
   XhibitBatchesScheduler xhibitBatchesScheduler;
 
   @PostMapping("/appeal-data-processing")
+  @Operation(description = "Trigger the Xhibit Batches Appeal Data Processing scheduled task")
+  @StandardApiResponse
   public ResponseEntity<String> triggerAppealDataProcessing() {
     xhibitBatchesScheduler.executeAppealDataProcessing();
     return ResponseEntity.ok("Xhibit Batches Appeal Data Processing task triggered.");
   }
 
   @PostMapping("/trial-data-processing")
+  @Operation(description = "Trigger the Xhibit Batches Trial Data Processing scheduled task")
+  @StandardApiResponse
   public ResponseEntity<String> triggerTrialDataProcessing() {
     xhibitBatchesScheduler.executeTrialDataProcessing();
     return ResponseEntity.ok("Xhibit Batches Trial Data Processing task triggered.");
