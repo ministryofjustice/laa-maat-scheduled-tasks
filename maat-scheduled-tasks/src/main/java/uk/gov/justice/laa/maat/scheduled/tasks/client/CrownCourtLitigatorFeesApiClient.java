d45a8cc5ab6189cb797d593e519e8569fc12e2b7
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2025-10-03 13:41:36 +0100

    reset cclf client to return void as not need to handle the response until 207 changes are in

--- maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/client/CrownCourtLitigatorFeesApiClient.java
+++ maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/client/CrownCourtLitigatorFeesApiClient.java
@@ -1,6 +1,5 @@
 package uk.gov.justice.laa.maat.scheduled.tasks.client;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
@@ -11,9 +10,9 @@ import uk.gov.justice.laa.maat.scheduled.tasks.request.UpdateRepOrdersRequest;
 @HttpExchange
 public interface CrownCourtLitigatorFeesApiClient {
     @PostExchange("/defendants")
-    ResponseEntity<String> updateApplicants(@RequestBody UpdateApplicantsRequest applicantsRequest);
+    void updateApplicants(@RequestBody UpdateApplicantsRequest applicantsRequest);
     @PostExchange("/defendant-histories")
-    ResponseEntity<String> updateApplicantsHistory(@RequestBody UpdateApplicantHistoriesRequest applicantHistoriesRequest);
+    void updateApplicantsHistory(@RequestBody UpdateApplicantHistoriesRequest applicantHistoriesRequest);
     @PostExchange("/rep-orders")
-    ResponseEntity<String> updateRepOrders(@RequestBody UpdateRepOrdersRequest repOrdersRequest);
+    void updateRepOrders(@RequestBody UpdateRepOrdersRequest repOrdersRequest);
 }
