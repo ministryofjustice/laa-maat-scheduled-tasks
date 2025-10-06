5b8b12b8cd3cbea6506a68290b6d6cb9a58f39ef
Karl Baker <karl.baker@digital.justice.gov.uk>
2025-10-06 14:44:28 +0100

    Refactor common fields into new BillingDTO class
    Co-Authored-By: Nigel Pain <nigel.pain@justice.gov.uk>

---
+++ maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/dto/BillingDTO.java
@@ -0,0 +1,35 @@
+package uk.gov.justice.laa.maat.scheduled.tasks.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+public class BillingDTO {
+
+    @NotNull
+    @JsonProperty("id")
+    private Integer id;
+
+    @NotNull
+    @JsonProperty("date_created")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS")
+    private LocalDateTime dateCreated;
+
+    @NotNull
+    @JsonProperty("user_created")
+    private String userCreated;
+
+    @JsonProperty("date_modified")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS")
+    private LocalDateTime dateModified;
+
+    @JsonProperty("user_modified")
+    private String userModified;
+}
