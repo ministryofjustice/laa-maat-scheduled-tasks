bb261c45f7b5d51a108628ee50169d9371eb4d7d
Karl Baker <karl.baker@digital.justice.gov.uk>
2025-10-13 15:44:00 +0100

    Refactor retrieval and storage of billing entities
    This commit refactors the _BillingDataFeedLogService_
    and underlying repository to allow entities to be
    retrieved from the database and to be able to save any
    type of entity that extends _BillingDTO_ using a single
    method via generics.

--- maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/repository/BillingDataFeedLogRepository.java
+++ maat-scheduled-tasks/src/main/java/uk/gov/justice/laa/maat/scheduled/tasks/repository/BillingDataFeedLogRepository.java
@@ -1,11 +1,15 @@
 package uk.gov.justice.laa.maat.scheduled.tasks.repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.justice.laa.maat.scheduled.tasks.entity.BillingDataFeedLogEntity;
+import uk.gov.justice.laa.maat.scheduled.tasks.enums.BillingDataFeedRecordType;
 
 @Repository
 public interface BillingDataFeedLogRepository extends JpaRepository<BillingDataFeedLogEntity, Integer> {
+    List<BillingDataFeedLogEntity> getBillingDataFeedLogEntitiesByRecordType(String recordType);
+
     Long deleteByDateCreatedBefore(LocalDateTime date);
 }
