da443ee067ac230a57b20b11f1555b4533ad98e3
Karl Baker <karl.baker@digital.justice.gov.uk>
2025-10-31 12:32:07 +0000

    Refactor retrieval and storage of billing entities
    This commit refactors the _BillingDataFeedLogService_
    and underlying repository to allow entities to be
    retrieved from the database and to be able to save any
    type of entity that extends _BillingDTO_ using a single
    method via generics.

--- maat-scheduled-tasks/src/test/java/uk/gov/justice/laa/maat/scheduled/tasks/service/BillingDataFeedLogServiceTest.java
+++ maat-scheduled-tasks/src/test/java/uk/gov/justice/laa/maat/scheduled/tasks/service/BillingDataFeedLogServiceTest.java
@@ -2,6 +2,7 @@ package uk.gov.justice.laa.maat.scheduled.tasks.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,6 +20,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.justice.laa.maat.scheduled.tasks.dto.ApplicantBillingDTO;
 import uk.gov.justice.laa.maat.scheduled.tasks.entity.BillingDataFeedLogEntity;
 import uk.gov.justice.laa.maat.scheduled.tasks.enums.BillingDataFeedRecordType;
+import uk.gov.justice.laa.maat.scheduled.tasks.mapper.BillingDataFeedLogMapper;
 import uk.gov.justice.laa.maat.scheduled.tasks.repository.BillingDataFeedLogRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,33 +30,67 @@ public class BillingDataFeedLogServiceTest {
     public static final LocalDateTime THRESHOLD_DATE = LocalDateTime.of(2025, 8, 1, 10, 0);
 
     @Mock
-    private BillingDataFeedLogRepository billingDataFeedLogRepository;
+    private BillingDataFeedLogMapper billingDataFeedLogMapper;
     @Mock
-    private ObjectMapper objectMapper;
+    private BillingDataFeedLogRepository billingDataFeedLogRepository;
     @InjectMocks
     private BillingDataFeedLogService billingDataFeedLogService;
 
     @Test
-    void givenValidData_whenSaveBillingDataFeedIsInvoked_thenDataIsSavedToRepository() throws JsonProcessingException {
-        BillingDataFeedRecordType recordType = BillingDataFeedRecordType.APPLICANT;
-        List<ApplicantBillingDTO> billingDTO = List.of(getApplicantDTO(TEST_ID));
+    void givenNoDataExists_whenGetBillingDataFeedLogsIsInvoked_thenReturnEmptyList() {
+        List<BillingDataFeedLogEntity> records = billingDataFeedLogService.getBillingDataFeedLogs(BillingDataFeedRecordType.APPLICANT);
 
-        when(objectMapper.writeValueAsString(billingDTO)).thenReturn(
-            "[ApplicantBillingDTO(id=1, firstName='test-first-name', lastName='test-last-name', " +
-                "otherNames='test-other-names', dob='2025-07-31', gender='male', niNumber='AB123456C', " +
-                "foreignId='foreign-ID', dateCreated='2024-08-29T11:38:12', userCreated='TEST', " +
-                "dateModified='2024-04-01T10:45:09', userModified='TEST')]"
-        );
-
-        billingDataFeedLogService.saveBillingDataFeed(recordType, billingDTO);
-
-        verify(billingDataFeedLogRepository).save(any(BillingDataFeedLogEntity.class));
+        assertThat(records).isEmpty();
     }
 
     @Test
+    void givenNoMatchingDataExists_whenGetBillingDataFeedLogsIsInvoked_thenReturnEmptyList() {
+        BillingDataFeedLogEntity repOrderBillingEntity = BillingDataFeedLogEntity.builder()
+            .id(1)
+            .recordType(BillingDataFeedRecordType.REP_ORDER.getValue())
+            .payload("")
+            .build();
+
+        lenient().when(billingDataFeedLogRepository.getBillingDataFeedLogEntitiesByRecordType(BillingDataFeedRecordType.REP_ORDER.getValue()))
+            .thenReturn(List.of(repOrderBillingEntity));
+
+        List<BillingDataFeedLogEntity> records = billingDataFeedLogService.getBillingDataFeedLogs(BillingDataFeedRecordType.APPLICANT);
+
+        assertThat(records).isEmpty();
+    }
+
+    @Test
+    void givenMatchingDataExists_whenGetBillingDataFeedLogsIsInvoked_thenReturnsRecords() {
+        BillingDataFeedLogEntity applicantBillingEntity = BillingDataFeedLogEntity.builder()
+            .id(123)
+            .recordType(BillingDataFeedRecordType.APPLICANT.getValue())
+            .payload("")
+            .build();
+
+        when(billingDataFeedLogRepository.getBillingDataFeedLogEntitiesByRecordType(BillingDataFeedRecordType.APPLICANT.getValue()))
+            .thenReturn(List.of(applicantBillingEntity));
+
+        List<BillingDataFeedLogEntity> records = billingDataFeedLogService.getBillingDataFeedLogs(BillingDataFeedRecordType.APPLICANT);
+
+        assertThat(applicantBillingEntity).isEqualTo(records.getFirst());
+    }
+
+    @Test
+    void givenValidData_whenSaveBillingDataFeedIsInvoked_thenDataIsSavedToRepository()
+        throws JsonProcessingException {
+        BillingDataFeedRecordType recordType = BillingDataFeedRecordType.APPLICANT;
+        List<ApplicantBillingDTO> applicantBillingDTOs = List.of(getApplicantDTO(1));
+
+        when(billingDataFeedLogMapper.mapDtoToEntity(recordType, applicantBillingDTOs)).
+            thenReturn(BillingDataFeedLogEntity.builder().build());
+
+        billingDataFeedLogService.saveBillingDataFeed(recordType, applicantBillingDTOs);
+
+        verify(billingDataFeedLogRepository).save(any(BillingDataFeedLogEntity.class));
+    }
+    @Test
     void givenADate_whenDeleteLogsBeforeDateInvoked_thenDeleteByDateCreatedBeforeIsInvoked() {
         when(billingDataFeedLogRepository.deleteByDateCreatedBefore(THRESHOLD_DATE)).thenReturn(2L);
-
         Long logsDeleted = billingDataFeedLogService.deleteLogsBeforeDate(THRESHOLD_DATE);
 
         verify(billingDataFeedLogRepository).deleteByDateCreatedBefore(THRESHOLD_DATE);
@@ -63,9 +99,8 @@ public class BillingDataFeedLogServiceTest {
 
     @Test
     void givenNoDate_whenDeleteLogsBeforeDateInvoked_thenAnIllegalArgumentExceptionIsThrown() {
-        assertThatThrownBy(() -> {
-            billingDataFeedLogService.deleteLogsBeforeDate(null);
-        }).isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> billingDataFeedLogService.deleteLogsBeforeDate(null))
+            .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("A date must be provided for the logs to be deleted.");
     }
 }
